7dacc852e8b5df553c2effdbf2bff8610be131ce
Toshiaki Maki <tmaki@vmware.com>
2023-03-02 09:01:20 +0900

    Change the order of propertySources

--- src/main/java/am/ik/utils/spring/DatabaseUrlEnvironmentPostProcessor.java
+++ src/main/java/am/ik/utils/spring/DatabaseUrlEnvironmentPostProcessor.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import am.ik.utils.DatabaseUrlParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
@@ -13,7 +15,7 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.MutablePropertySources;
 
-import static org.springframework.core.env.CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME;
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
 
 public class DatabaseUrlEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
 
@@ -21,12 +23,15 @@ public class DatabaseUrlEnvironmentPostProcessor implements EnvironmentPostProce
 
 	public static final String SOURCE_NAME = "databaseUrlProperties";
 
+	private final Logger log = LoggerFactory.getLogger(DatabaseUrlEnvironmentPostProcessor.class);
+
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 		final String databaseUrl = (String) environment.getSystemEnvironment().get(DATABASE_URL_KEY);
 		if (databaseUrl == null) {
 			return;
 		}
+		log.info("Detect '{}' environment variable", DATABASE_URL_KEY);
 		final DatabaseUrlParser urlParser = new DatabaseUrlParser(databaseUrl);
 		final Map<String, Object> properties = new TreeMap<>();
 		properties.put("spring.datasource.url", urlParser.getJdbcUrl());
@@ -34,8 +39,8 @@ public class DatabaseUrlEnvironmentPostProcessor implements EnvironmentPostProce
 		properties.put("spring.datasource.password", urlParser.getPassword());
 		final MapPropertySource propertySource = new MapPropertySource(SOURCE_NAME, properties);
 		final MutablePropertySources propertySources = environment.getPropertySources();
-		if (propertySources.contains(COMMAND_LINE_PROPERTY_SOURCE_NAME)) {
-			propertySources.addAfter(COMMAND_LINE_PROPERTY_SOURCE_NAME, propertySource);
+		if (propertySources.contains(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME)) {
+			propertySources.addBefore(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, propertySource);
 		}
 		else {
 			propertySources.addFirst(propertySource);
